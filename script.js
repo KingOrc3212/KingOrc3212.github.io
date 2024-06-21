0dde161f4895274d863b367af3c10463dfbdbd6d
KingOrc3212 <157957742+KingOrc3212@users.noreply.github.com>
2024-06-21 14:43:31 +0300

    Summary;
    the final form..

--- script.js
+++ script.js
@@ -1,56 +1,59 @@
-/*let randomNum = Math.floor(Math.random() * 100) + 1;
+// number guessing game code
 
-//if number is lower or higher
-const lowhi = document.querySelector(".highlow");
-// number which User guesses
-const guessednumber = document.querySelector(".numberGuess");
-// how many guesses are left
-const guess = document.querySelector("remainingGuesses")
-// submit the stuff
-const submit = document.querySelector(".checkBtn");
-
-console.log(randomNum)
-
-if (randomNum == guessednumber) {
-    lowhi.innerHTML = 'You guessed right!! the number was ';
-    console.log("kakka1");
-}
-else if (randomNum < guessednumber) {
-    lowhi.innerHTML = 'Too high';
-    console.log("kakka2");
-}
-else if (randomNum > guessednumber) {
-    lowhi.innerHTML = 'Too low';
-    console.log("kakka3");
-}*/
-
-const minNum = 1;
-const maxNum = 100;
-const ranNum = Math.floor(Math.random() * (maxNum - minNum));
 const checkBtn = document.getElementById("checkBtn");
-const retryBtn = document.getElementById("retryBtn")
+const retryBtn = document.getElementById("retryBtn");
 
+let ranNum = randomNumberGenerator();
 let guess = document.getElementById("guess");
 let guesses = 10;
 let remainingGuess = document.getElementById("remainingGuesses");
 let highlow = document.getElementById("highlow");
+let text = document.getElementById("guessedNum");
 
-let right = true;
-
-console.log(ranNum);
+function randomNumberGenerator() {
+    const num = Math.floor(Math.random() * (100 - 1));
+    return num;
+}
 
 checkBtn.addEventListener("click", function(){
-    console.log("Button was pressed");
-    if(guesses > 0) {
-        guesses --;
-        remainingGuess.innerHTML = guesses;
-    }
-    if(guesses == 0) {
-        highlow.innerHTML = "Game Over";
+    const inputValue = guess.value;
+    const value = parseInt(inputValue);
+
+    if(isNaN(value) == false){
+        prevGuesses = guesses;
+
+        if(inputValue > ranNum) {
+            highlow.innerHTML = "Your guess is too high";
+        }
+        if(inputValue < ranNum) {
+            highlow.innerHTML = "Your guess is too low";
+        }
+        if(inputValue == ranNum) {
+            highlow.innerHTML = "Your Guess is correct! You win";
+        }
+    
+        if(guesses > 0) {
+            guesses --;
+            remainingGuess.innerHTML = guesses;
+        }
+        if(prevGuesses > 0) {
+            text.textContent += guess.value + " ";
+        }
+        if(guesses == 0) {
+            highlow.innerHTML = "Game Over";
+        }
+
+    } else if(isNaN(value) == true) {
+        highlow.innerHTML = "Add a number to the input field";
     }
     
 });
 
 retryBtn.addEventListener("click", function(){
-    
+    guess.value = "";
+    guesses = 10;
+    remainingGuess.innerHTML = "10";
+    text.innerHTML = ""
+    highlow.innerHTML = "Hints are shown here";
+    ranNum = randomNumberGenerator();
 });
